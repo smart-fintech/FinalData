10d2b2ed6ae30ba405c29b7d104a08a2adeebb90
smart-fintech <77914461+smart-fintech@users.noreply.github.com>
2021-10-20 15:47:35 +0530

    Updated code

--- tallyapp/serializers.py
+++ tallyapp/serializers.py
@@ -18,3 +18,9 @@ class UpdateCompanySerializer(serializers.ModelSerializer):
         model = companydata
         fields = ['id','comp_name','comp_email', 'comp_phone', 'comp_address',
         'comp_state','comp_website','comp_gstin','mac_ad','vat_no','cst_no','pan_no',]        
+
+class UpdateLegderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ladgernamedata
+        fields = ['id','ledeger_name','ledeger_email', 'ledeger_phone', 'ledeger_address',
+        'ledeger_state','ledeger_website','ledeger_gstin']    
