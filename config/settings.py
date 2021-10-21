bfc46f5eb99e0ca5b27eb9ee5b22779f8d594dc6
smart-fintech <77914461+smart-fintech@users.noreply.github.com>
2021-10-21 11:56:02 +0530

    Change DB

--- config/settings.py
+++ config/settings.py
@@ -102,26 +102,26 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 CORS_ORIGIN_ALLOW_ALL=True
 ALLOWED_HOSTS=['*']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fintech', 
-        'USER': 'postgres', 
-        'PASSWORD':'admin123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'fintech', 
+#         'USER': 'postgres', 
+#         'PASSWORD':'admin123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
