10e317b2a917ecb808739089ec62b6a91617b021
smart-fintech <77914461+smart-fintech@users.noreply.github.com>
2021-11-11 17:41:40 +0530

    Donw

--- config/settings.py
+++ config/settings.py
@@ -111,7 +111,25 @@ DATABASES = {
 
 CORS_ORIGIN_ALLOW_ALL=True
 ALLOWED_HOSTS=['*']
-
+CORS_ALLOW_METHODS = [
+'DELETE',
+'GET',
+'OPTIONS',
+'PATCH',
+'POST',
+'PUT',
+]
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
+]
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
