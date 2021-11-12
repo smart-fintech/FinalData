b20d30bf27fe06904871ce6c56b777b97c5f98c9
smart-fintech <77914461+smart-fintech@users.noreply.github.com>
2021-11-12 12:33:15 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -63,8 +63,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     # cors middleware
-    #'corsheaders.middleware.CorsMiddleware',
-    #'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
