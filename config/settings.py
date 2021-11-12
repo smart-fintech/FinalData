5a1dd0f359b8388b459f267d93b223989f62cf00
smart-fintech <77914461+smart-fintech@users.noreply.github.com>
2021-11-12 11:16:20 +0530

    Changed

--- config/settings.py
+++ config/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+CORS_ALLOW_CREDENTIALS=True
 CORS_ORIGIN_ALLOW_ALL=True
 ALLOWED_HOSTS=['*']
 CORS_ALLOW_METHODS = [
