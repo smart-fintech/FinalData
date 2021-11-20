11e327ace7bc315322143fe5326fa0a65f6a99d5
smart-fintech <77914461+smart-fintech@users.noreply.github.com>
2021-11-20 09:15:35 +0530

    Update urls.py

--- tallyapp/urls.py
+++ tallyapp/urls.py
@@ -6,17 +6,15 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('postlegder/', views.LegderPost.as_view()),
+    path('postvoucher/', views.VoucherPost.as_view()),
     path('api/fetchladeger/', views.ladegerList.as_view()),
     path('getcompanynamedetails/', views.CompanyList.as_view()),
     path('getcompanydetails/', views.NormalCompanyList.as_view()),
     path('UpdateCompany/<int:pk>/', views.UpdateCompany.as_view()),
     path('UpdateLegder/<int:pk>/', views.UpdateLegder.as_view()),
     path('getladeger', views.get_ledeger, name='getladeger'),
-    # path('createladeger', views.create_ledger, name='createladeger'),
-    # path('createladeger',views.create_ladeger,name='createladeger'),
     path('voucher',views.voucher_entery, name='voucher'),
     path('getcompanyname',views.get_company_name, name='getcompanyname'),
     path('grpname',views.creategrup, name='grpname'),
     path('createladegerwithcompany',views.create_ladeger_with_company, name='createladegerwithcompany'),
-    # path('get_voucher',views.led_statement,name="get_voucher")
 ]
