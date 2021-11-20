939ba0f8c4dc1f1eac2b6c549eaf9fb0e037fc0e
smart-fintech <77914461+smart-fintech@users.noreply.github.com>
2021-11-20 09:13:48 +0530

    Update admin.py

--- tallyapp/admin.py
+++ tallyapp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import ladgernamedata,companydata
+from .models import ladgernamedata,companydata,voucherfromtally
 
 
 admin.site.register(ladgernamedata)
 admin.site.register(companydata)
+admin.site.register(voucherfromtally)
 
 # admin.site.register(PpBnkM)
 
