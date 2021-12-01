b340d5dd14e6b7af76a4cf285b728a5445abc5de
smart-fintech <77914461+smart-fintech@users.noreply.github.com>
2021-12-01 11:18:33 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,8 @@
 cd /home/ubuntu/smartdotcheck
 
+# Download config
+aws s3 cp s3://intelligere-configs/production/production.env .env
+
 # nohup gunicorn config.wsgi >& /dev/null &
 # gunicorn config.wsgi --daemon
 nohup gunicorn config.wsgi >& /dev/null &
